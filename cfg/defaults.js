1b4d80f5e93c7aced1ce9939957bc93674743e9c
laihaibo <18768101327@163.com>
2016-09-06 23:02:56 +0800

    fix path problems

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -53,7 +53,7 @@ function getDefaultModules() {
 
 module.exports = {
     srcPath: srcPath,
-    publicPath: '/assets/',
+    publicPath: './assets/',
     port: dfltPort,
     getDefaultModules: getDefaultModules
 };
